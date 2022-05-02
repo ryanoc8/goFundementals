5b8d3c9d9c08b5e061ddda2f647b8eae58dc73d6
ryanoc8 <ryanroc8@gmail.com>
2022-05-02 13:32:26 +0100

    added chapter 3 functions, need to fix error on excercise

---
+++ 03_Functions/write_a_function.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// contentType will return the value of Content-Type header returned by making a
+// HTTP GET requst to a url
+
+func contentType(url string) (string, error) {
+	resp, err := http.Get("url")
+
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close() // Make sure we close the body.
+
+	ctype := resp.Header.Get("Content-Type")
+	if ctype == "" { // Return error if Content-Type header not found.
+		return "", fmt.Errorf("Can't find Content-Type header")
+	}
+
+	return ctype, nil
+}
+
+func main() {
+	ctype, err := contentType("https://linkedin.com")
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+	} else {
+		fmt.Println(ctype)
+	}
+}
